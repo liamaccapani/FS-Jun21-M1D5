55cb79b28bfae830fdde000c17d1ffd3d9be4f6d
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-06-25 15:18:45 +0200

    Completed exercise 1

---
+++ homeworkD5.js
@@ -0,0 +1,20 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+*/
+
+/* EXERCISE 1
+Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
+*/
+
+const area = function(l1, l2) {
+    let area = (l1 * l2)
+    return area
+}
+
+let result = area(3, 4)
+console.log(result)
+
